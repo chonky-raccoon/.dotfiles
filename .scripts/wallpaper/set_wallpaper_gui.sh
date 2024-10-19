bbf485c6f6e964930cb7ea1b714a4c08b02daec9
chonky-raccoon <rcj.dev@proton.me>
2024-10-19 19:33:19 +0200

    Have zenity open on wallpapers dir

--- .scripts/wallpaper/set_wallpaper_gui.sh
+++ .scripts/wallpaper/set_wallpaper_gui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use zenity to open a file chooser dialog and get the selected file
-selected_file=$(zenity --file-selection --title="Select an Image" --filename="$HOME/")
+selected_file=$(zenity --file-selection --title="Select an Image" --filename="$HOME/Pictures/wallpapers/wallpaper.jpg")
 
 # Check if the user canceled the dialog
 if [ -z "$selected_file" ]; then
